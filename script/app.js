7b2f359bc525832a97f73af57febfa3e9f5f09e5
Blue Sky Empires <30746317+SidharthRai@users.noreply.github.com>
2017-08-31 19:20:09 +0530

    REQUIRED

--- script/app.js
+++ script/app.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker
-    .register('/sw.js')
+    .register('./sw.js')
     .then(function(registration) {
       console.log("[Service Worker] Registered");
     })
